769f53418ff0b7abbf0ffe2e3afa22165ba50229
Michael Marner <michael.marner@unisa.edu.au>
2013-06-14 07:03:47 +0930

    Show profile.

--- single-threed_show.php
+++ single-threed_show.php
@@ -16,23 +16,41 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
+	<div id="primary" class="site-content-full">
 		<div id="content" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
-			<?php the_post_thumbnail(); ?>
-				<?php get_template_part( 'content', 'page' ); ?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</header>
+<?php
+			if (has_post_thumbnail(get_the_ID())) {
+				echo the_post_thumbnail('threed-show-profile');
+			}
+			else {
+				echo '<div class="show-profile-image">';
+					echo '<img src="';
+				bloginfo('template_directory');
+				echo '/images/NoImage.png" alt="No Show Art"/>';
+				echo '</div>';
+			}
+?>
+<div style="float:left; width:580px; padding-left: 48px;">
+		<div class="entry-content">
+			<?php the_content(); ?>
 <p><b>Hosted By:</b> <?php echo get_post_meta(get_the_ID(), 'threed_show_hosts', true); ?></p>
 <p><b>Airs On: </b> <?php echo threedGetDay(get_post_meta(get_the_ID(), 'threed_show_day', true)); ?>, 
 <?php
 echo threedFriendlyTime( get_post_meta(get_the_ID(), 'threed_show_start', true)) . " - ";
 echo threedFriendlyTime( get_post_meta(get_the_ID(), 'threed_show_end', true)) ;
 ?></p>
-<p><b><a href="<?php echo get_post_meta(get_the_ID(), 'threed_show_url', true); ?>">Show Website</a></b></p>
+		</div><!-- .entry-content -->
+</div>
+	</article><!-- #post -->
 
-				<?php
 
-?>
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
