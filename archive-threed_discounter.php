9f79f8362820afe8fcc3fb908d284f234e722d3e
Michael Marner <michael.marner@unisa.edu.au>
2013-06-27 19:19:48 +0930

    Tweaking discounters.

--- archive-threed_discounter.php
+++ archive-threed_discounter.php
@@ -22,20 +22,13 @@ get_header(); ?>
 	<section id="primary" class="site-content">
 		<div id="content" role="main">
 
-		<?php if ( have_posts() ) : ?>
-			<header class="archive-header">
-				<h1 class="archive-title"><?php
-					if ( is_day() ) :
-						printf( __( 'Daily Archives: %s', 'twentytwelve' ), '<span>' . get_the_date() . '</span>' );
-					elseif ( is_month() ) :
-						printf( __( 'Monthly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentytwelve' ) ) . '</span>' );
-					elseif ( is_year() ) :
-						printf( __( 'Yearly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentytwelve' ) ) . '</span>' );
-					else :
-						_e( 'Archives', 'twentytwelve' );
-					endif;
-				?></h1>
-			</header><!-- .archive-header -->
+		<header class="entry-header">
+			<h1 class="entry-title">Discounters</h1>
+		</header>
+
+		<div class="entry-content">
+<p>Three D subscribers are eligible for VIP treatment from a wide range of businesses around South Australia. The current discounters are listed below, simply show your subscriber card! Our Discounters are ever changing, so check back here often for the latest deals.</p>
+</div>
 
 			<?php
 			/* Start the Loop */
@@ -45,16 +38,31 @@ get_header(); ?>
 				 * this in a child theme then include a file called called content-___.php
 				 * (where ___ is the post format) and that will be used instead.
 				 */
-				get_template_part( 'content', get_post_format() );
+?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php
+			if (has_post_thumbnail(get_the_ID())) {
+				echo the_post_thumbnail('discounter-thumb');
+			}
+			else {
+				echo '<div class="attachment-discounter-thumb"></div>';
+			}
+?>
+<div style="float:left; width:520px; padding-left: 24px;">
+		<div class="entry-content">
+			<h2 class="entry-title "><?php the_title(); ?></h2>
+<p><?php echo get_post_meta(get_the_ID(), 'threed_discounter_discount', true); ?></p>
+		</div><!-- .entry-content -->
+</div>
+	</article><!-- #post -->
+<div style="clear: both;"></div>
 
+<?php
 			endwhile;
 
 			twentytwelve_content_nav( 'nav-below' );
 			?>
 
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		<?php endif; ?>
 
 		</div><!-- #content -->
 	</section><!-- #primary -->
